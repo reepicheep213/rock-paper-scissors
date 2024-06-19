6dac9a10fc462b3b9b0617ec90778863905d2612
Jeff Ro <44447450+reepicheep213@users.noreply.github.com>
2024-06-19 16:03:53 -0700

    Include functions for Steps 2 and 3

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# rock-paper-scissors
+# The Odin Project - Rock Paper Scissors Game
+
+## Project Description
+
+The project is to create a simple rock paper scissors game that can be played in the console using the early JavaScript concepts learned in the Foundations Course at The Odin Project.
+
+## Learning Objectives
+
+The objective is to apply JavaScript, HTML, and CSS basics as well as to practice problem-solving and researching skills.
+
+## Aditional Information
---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Step 2: Write the logic to get the computer choice
+// Get a random value among rock, paper, and scissors
+
+function getComputerChoice() {
+  let cpuWeapon = (Math.random() * 3);
+  if (cpuWeapon >= 2) {
+    return "Rock";
+  } else if (cpuWeapon >= 1 && cpuWeapon < 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+// console.log(getComputerChoice()); / tested 
+
+// Step 3: Write the logic to get the human choice
+// When the user inputs a value
+// Get one of the valid choices depending on the user's input
+
+function getHumanChoice() {
+  let humanWeapon = prompt("Enter \"1\" for Rock, \"2\" for Paper, or \"3\" for Scissors");
+  if (humanWeapon == 1) {
+    return "Rock";
+  } else if (humanWeapon == 2) {
+    return "Paper";
+  } else if (humanWeapon == 3) {
+    return "Scissors";
+  } else {
+    return "Invalid";
+  }
+}
+
+console.log(getHumanChoice());
