b8e41d79008b76a4fcaed9692e1d01a251a365f3
reepicheep213 <44447450+reepicheep213@users.noreply.github.com>
2024-06-20 14:52:33 -0700

    Increment the score

--- js/script.js
+++ js/script.js
@@ -50,20 +50,27 @@ function playRound(humanChoice, computerChoice) {
   console.log('You chose ' + humanChoice + '.\nComputer chose ' + computerChoice+ '.');
   if (humanChoice === 'rock' && computerChoice === 'scissors') {
     console.log('You win! Rock beats scissors.');
+    ++humanScore;
   } else if (humanChoice === 'rock' && computerChoice === 'paper') {
     console.log('You lose! Paper beats rock.'); 
+    ++computerScore;
   } else if (humanChoice === 'paper' && computerChoice === 'rock') {
     console.log('You win! Paper beats rock.');
+    ++humanScore;
   } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-    console.log('You lose! Scissors beats Paper.'); 
+    console.log('You lose! Scissors beats Paper.');
+    ++computerScore; 
   } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-    console.log('You win! Scissors beats paper');
+    console.log('You win! Scissors beats paper.');
+    ++humanScore;
   } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-    console.log('You lose! Rock beats scissors.'); 
+    console.log('You lose! Rock beats scissors.');
+    ++computerScore; 
   } else if (humanChoice === computerChoice) {
     console.log('It\'s a tie.')
   } else {
     console.log('"' + humanSelection + '"' + ' is invalid. You lose by default.');
+    ++computerScore;
   }
 }
 
