ec373a9fd5dbe7bf9244f07fd22be0149efe8272
reepicheep213 <44447450+reepicheep213@users.noreply.github.com>
2024-06-20 14:03:04 -0700

    Clean up playRound to fewer lines

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@
 function getComputerChoice() {
   const randomNumber = (Math.random() * 3);
   if (randomNumber >= 2) {
-    return "Rock";
+    return 'rock';
   } else if (randomNumber >= 1) {
-    return "Paper";
+    return 'paper';
   } else {
-    return "Scissors";
+    return 'scissors';
   }
 }
 // console.log(getComputerChoice()); // tested 
@@ -19,15 +19,15 @@ function getComputerChoice() {
 // Get one of the valid choices depending on the user's input
 
 function getHumanChoice() {
-  const userWeapon = prompt("Choose your weapon: Rock, Paper, or Scissors").toLowerCase();
-  if (userWeapon == "rock") {
-    return "Rock";
-  } else if (userWeapon == "paper") {
-    return "Paper";
-  } else if (userWeapon == "scissors") {
-    return "Scissors";
+  const userWeapon = prompt('Choose your weapon: Rock, Paper, or Scissors').toLowerCase();
+  if (userWeapon == 'rock') {
+    return 'rock';
+  } else if (userWeapon == 'paper') {
+    return 'paper';
+  } else if (userWeapon == 'scissors') {
+    return 'scissors';
   } else {
-    return "Invalid";
+    return userWeapon;
   }
 }
 
@@ -37,8 +37,8 @@ function getHumanChoice() {
 // Step 4: Declare the players score variables
 // Keep track of the user and computer scores using variables
 
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 // Step 5: Write the logic to play a single round
 // Take human and computer choices as arguments
@@ -47,33 +47,26 @@ const computerScore = 0;
 // Log a winner announcement
 
 function playRound(humanChoice, computerChoice) {
-  console.log("You chose " + humanChoice + "\nComputer chose " + computerChoice);
-  if (humanChoice == "Rock") {
-    if (computerChoice == "Scissors") {
-      console.log("You win! Rock beats Scissors.");
-    } else if (computerChoice == "Paper") {
-      console.log("You lose! Paper beats Rock."); 
-    } else {
-      console.log("It's a tie.");
-    };
-  } else if (humanChoice == "Paper") {
-    if (computerChoice == "Rock") {
-      console.log("You win! Paper beats Rock.");
-    } else if (computerChoice == "Scissors") {
-      console.log("You lose! Scissors beats Paper."); 
-    } else {
-      console.log("It's a tie.");
-    };
-  } else if (humanChoice == "Scissors") {
-    if (computerChoice == "Paper") {
-      console.log("You win! Scissors beats Paper.");
-    } else if (computerChoice == "Rock") {
-      console.log("You lose! Rock beats Scissors."); 
-    } else {
-      console.log("It's a tie.");
-    };
-  };
-};
+  console.log('You chose ' + humanChoice + '.\nComputer chose ' + computerChoice+ '.');
+  if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    console.log('You win! Rock beats scissors.');
+  } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+    console.log('You lose! Paper beats rock.'); 
+  } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    console.log('You win! Paper beats rock.');
+  } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+    console.log('You lose! Scissors beats Paper.'); 
+  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    console.log('You win! Scissors beats paper');
+  } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+    console.log('You lose! Rock beats scissors.'); 
+  } else if (humanChoice === computerChoice) {
+    console.log('It\'s a tie.')
+  } else {
+    console.log('"' + humanSelection + '"' + ' is invalid. You lose by default.');
+  }
+}
+
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
